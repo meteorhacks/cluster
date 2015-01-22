67812f725be7327c7bc102b7f1e6e17e37b9354d
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-01-22 15:01:18 +0530

    adding load balancing support

--- package.js
+++ package.js
@@ -5,6 +5,19 @@ Package.describe({
   "name": "meteorhacks:cluster-manager"
 });
 
+Npm.depends({
+  "cookies": "0.5.0"
+});
+
+Package.onTest(function(api) {
+  configurePackage(api);
+  api.use('tinytest');
+
+  api.addFiles([
+    'test/server/backends/mongo.js'
+  ], 'server');
+});
+
 Package.onUse(function(api) {
   configurePackage(api);
   api.export('ClusterManager');
@@ -13,6 +26,8 @@ Package.onUse(function(api) {
 function configurePackage(api) {
   api.versionsFrom('METEOR@0.9.2');
   api.use(['mongo-livedata', 'tracker', 'ddp'], ['server', 'client']);
+  api.use('meteorhacks:picker@1.0.1', 'server');
+  api.use('meteorhacks:inject-data@1.2.1')
 
   api.addFiles([
     'lib/namespace.js',
@@ -20,8 +35,11 @@ function configurePackage(api) {
   ], ['server', 'client']);
 
   api.addFiles([
+    'lib/server/backends/mongo.js',
+    'lib/server/backends/mongo_cursor.js',
     'lib/server/cursor.js',
-    'lib/server/api.js'
+    'lib/server/api.js',
+    'lib/server/load_balancing.js'
   ], ['server']);
 
   api.addFiles([
--- todo.md
+++ todo.md
@@ -6,12 +6,17 @@
 
 # LoadBalancing
 
-* Get the service url from the PUBLIC_URL
-* Inject the PUBLIC_URL if available by inject-data
+- * Get the service url from the PUBLIC_URL
+- * Inject the PUBLIC_URL if available by inject-data
+* Implement the new cursor with URL as the _id
+* Allow the give special meaning to the autoUpdateVersion when quering
+* send max of 2 or 3 URLS to the client to avoid loading issues
 * When we get the HTML page to the main server, get the current server asigned by the discovery
 * If it's not the current app, proxy the default HTML page from that site
   * set special header to avoid looping
   * set a cookie to proxy static requests from this server
+  * reset the cookie after sending the HTML page (we don't need to worry about setting the cookie via JS)
+  * We only need the cookie to proxy static content and other AJAX requests
 * Proxy static content
 * When the app is loading, connect to the public-url got from the InjectData
 * subscribe to new a discovery URL (after connected)
