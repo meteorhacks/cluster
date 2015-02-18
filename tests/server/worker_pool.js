f5ee1b55b00a0c102e976bb8765b07d697fca01e
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-02-18 15:04:53 +0530

    Add tests for the WorkerPool

--- tests/server/worker_pool.js
+++ tests/server/worker_pool.js
@@ -94,3 +94,82 @@ Tinytest.add("WorkerPool - pickWorker", function(test) {
 
   test.equal(worker.id, workerId);
 });
+
+Tinytest.addAsync("WorkerPool - _fork", function(test, done) {
+  var wp = new WorkerPool(0);
+  var child_process = Npm.require('child_process');
+  var portscanner = Npm.require('portscanner');
+
+  var port = 8000;
+  var p = {};
+  var fork = sinon.stub().returns(p);
+  var findAPortNotInUse = sinon.stub().callsArgWith(3, null, port);
+
+  WithNew(child_process, {fork: fork}, function() {
+    WithNew(portscanner, {findAPortNotInUse: findAPortNotInUse}, function() {
+      wp._fork(function(worker) {
+        test.equal(worker.id, wp._ids);
+        test.equal(worker.port, port);
+
+        test.isTrue(fork.called);
+        test.isTrue(findAPortNotInUse.called);
+        test.equal(worker.process, p);
+        done();
+      });
+    });
+  })
+});
+
+Tinytest.add("WorkerPool - _createWorker, check registration", function(test) {
+  var wp = new WorkerPool(0);
+  var worker = {
+    process: {
+      once: sinon.stub()
+    },
+    id: 100
+  };
+  wp._fork = sinon.stub().callsArgWith(0, worker);
+
+  wp._createWorker();
+
+  test.equal(wp._workersMap[worker.id], worker);
+  test.isTrue(wp._workers.indexOf(worker) >= 0);
+  test.isTrue(worker.process.once.called);
+  test.isTrue(wp._fork.called);
+});
+
+Tinytest.add("WorkerPool - _createWorker, on exit and closed!", function(test) {
+  var wp = new WorkerPool(0);
+  var worker = {
+    process: new (Npm.require('events').EventEmitter),
+    id: 100
+  };
+
+  wp._fork = sinon.stub().callsArgWith(0, worker);
+  wp._createWorker();
+  test.isTrue(wp._fork.called);
+
+  wp._closed = true;
+  worker.process.emit('exit');
+  test.equal(wp._workers.length, 0);
+  test.equal(wp._workersMap, {});
+});
+
+Tinytest.add(
+"WorkerPool - _createWorker, on exit and not closed!", function(test) {
+  var wp = new WorkerPool(0);
+  var worker = {
+    process: new (Npm.require('events').EventEmitter),
+    id: 100
+  };
+
+  wp._fork = sinon.stub().callsArgWith(0, worker);
+  wp._createWorker();
+  test.isTrue(wp._fork.called);
+
+  wp._closed = false;
+  wp._createWorker = sinon.stub();
+
+  worker.process.emit('exit');
+  test.isTrue(wp._createWorker.called)
+});
