493414e78af713f4f5c3d1f923d4ceb0ee7a81ea
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-02-18 14:07:32 +0530

    Bump versions

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   "summary": "Clustering solution for Meteor with load balancing and service discovery.",
-  "version": "1.4.0",
+  "version": "1.4.1",
   "git": "https://github.com/meteorhacks/cluster.git",
   "name": "meteorhacks:cluster"
 });
