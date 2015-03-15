fe2373933b699d9bfaa5a779df2f816347fa30ca
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-03-15 08:07:59 +0530

    Use a binary-less mongodb npm module

--- package.js
+++ package.js
@@ -9,7 +9,9 @@ Npm.depends({
   "cookies": "0.5.0",
   "http-proxy": "1.8.1",
   "portscanner": "1.0.0",
-  "mongodb": "1.4.33"
+  // mongodb@1.4.32 with binary modules removed!
+  // Stolen from Meteor's mongo package. see: http://goo.gl/GJJQ8e
+  "mongodb": "https://github.com/meteor/node-mongodb-native/tarball/634759e6326dc19a228df66ddb309285532f3b8a"
 });
 
 Package.onTest(function(api) {
